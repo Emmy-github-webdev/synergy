b4b0b7ccf0379b28c4823655f192216b16bcca05
Emmanuel <ogahemmanuel75@gmail.com>
2020-12-17 22:44:12 +0100

    edit packages

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ require('./routes/auth.routes')(app);
 require('./routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
